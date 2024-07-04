8f2093b0b4588276e928d0c720a623526880e2e2
aisha-06 <ayesha140406@gmail.com>
2024-07-04 18:22:27 +0530

    in process!

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
 function validate()
 {
-    var name=document.getElementById('name').value;
-    var password1=document.getElementById('password1').value;
-    var password2=document.getElementById('password2').value;
+    var name=document.getElementById('name');
+    var password1 =document.getElementById('password1');
+    var password2=document.getElementById('password2');
     var mobileno=document.getElementById('no').value;
     var email=document.getElementById('email-id').value;
 
     console.log('name')
 
-    if ((name==null)|| ( name==""))
+    if (name.value==null||  name.value=="")
     {    
          document.getElementById('uname').innerHTML="FILL THE USERNAME FIELD!";
          return false;
     }
-    else
+    if((name.value.length<=2)||(name.value.length>=30))
     {
-        document.getElementById('uname').innerHTML ="";
-        	return false;
+     document.getElementById('uname').innerHTML="LENGTH SHOULD BE BETWEEN 3 AND 30!";
+     return false;
     }
-    if((name))
+    if(!isNaN(name.value))
+    {
+     document.getElementById('uname').innerHTML="USERNAME SHOULD BE ONLY CHARACTER!";
+     return false;
+    }
+
+  
+
     
 }
